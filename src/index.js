380a6f987512015b5a4d5f8b21918a3aa41d9960
sarahcav90 <86289614+sarahcav90@users.noreply.github.com>
2021-07-17 17:50:20 +0200

    fix days

--- src/index.js
+++ src/index.js
@@ -21,6 +21,14 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
@@ -33,9 +41,11 @@ function displayForecast(response) {
       forecastHTML +
       `
     <div class="col-2">
-      <div class="forecast-day">${forecastDay.dt}</div>
+      <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
       <img
-        src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+        src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt=""
         width="45"
       />
